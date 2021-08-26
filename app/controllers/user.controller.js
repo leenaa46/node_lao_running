6aae8a772915f0004d00e1fbfc430b42f937fa2d
Billy <hsherlock22@gmail.com>
2021-08-26 18:25:12 +0700

    bug: resize

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -6,6 +6,9 @@ import Response from '../helpers/response.helper';
 import Status from '../helpers/status.helper';
 import Message from '../helpers/message.helper';
 import Cloudinary from '../utils/cloudinary'
+import Sharp from 'sharp'
+import fs from 'fs'
+import path from 'path'
 
 exports.register = async (req, res) => {
   const transaction = await db.sequelize.transaction();
@@ -22,6 +25,21 @@ exports.register = async (req, res) => {
     const is_active = package_id == 1 ? true : false;
     const status = package_id == 1 ? Status.userPackageStatus.Success : Status.userPackageStatus.Pending;
 
+    function resize(path, format, width, height) {
+      const readStream = fs.createReadStream(path)
+      let transform = Sharp()
+
+      if (format) {
+        transform = transform.toFormat(format)
+      }
+
+      if (width || height) {
+        transform = transform.resize(width, height)
+      }
+
+      return readStream.pipe(transform)
+    }
+
     const user = await db.User.create({
       name,
       phone,
@@ -29,19 +47,42 @@ exports.register = async (req, res) => {
       password: encryptedPassword,
       package_id,
       is_active: is_active
+    }, {
+      transaction: transaction
     });
 
     await db.UserProfile.create({
       user_id: user.id,
       name,
       surname
+    }, {
+      transaction: transaction
     })
 
     let cloudImage
     if (req.file) {
+      const {
+        filename: image
+      } = req.file;
+      console.log('path', req.file.path, req.file.destination, image);
 
-      cloudImage = await Cloudinary.uploader.upload(req.file.path)
-      console.log(cloudImage);
+
+      res.type(`image/jpeg`);
+
+      const result = resize(req.file.path, 'jpeg', 200, 200)
+
+      cloudImage = await Cloudinary.uploader.upload(result)
+      // const resizeImage = await Sharp(req.file.path)
+      //   .resize(200, 200)
+      //   .jpeg({
+      //     quality: 90
+      //   })
+      //   .toFile(
+      //     path.resolve(req.file.destination, 'resized', image)
+      //   )
+
+      // fs.unlinkSync(req.file.path)
+      // console.log(resizeImage);
     }
 
     await db.UserPackage.create({
@@ -49,6 +90,8 @@ exports.register = async (req, res) => {
       package_id,
       status: status,
       payment_slip: cloudImage.secure_url
+    }, {
+      transaction: transaction
     })
 
     const token = jwt.sign({
--- app/utils/multer.js
+++ app/utils/multer.js
@@ -2,7 +2,15 @@ import multer from "multer";
 import path from "path";
 
 module.exports = multer({
-  storage: multer.diskStorage({}),
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  }),
   fileFilter: (req, file, cb) => {
     // Create regex to match jpg and png
     const validFileTypes = /jpg|jpeg|png/
