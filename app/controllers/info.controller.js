94a8ebfdd8f3d5bcefd00dffd703dbc784dc891e
LeeNaa <hsherlock22@gmail.com>
2021-08-28 19:49:49 +0700

    Paginate package

--- app/controllers/info.controller.js
+++ app/controllers/info.controller.js
@@ -12,6 +12,30 @@ import Message from '../helpers/message.helper';
  */
 exports.findAll = async (req, res) => {
   try {
+    const per_page = Number.parseInt(req.query.per_page)
+    let page = Number.parseInt(req.query.page)
+
+    if (per_page) {
+      let packageData = {}
+      page = page && page > 0 ? page : 1
+
+      const packages = await db.Package.findAndCountAll({
+        limit: per_page,
+        offset: (page - 1) * per_page,
+        subQuery: false
+      })
+
+      packageData.data = packages.rows
+      packageData.pagination = {
+        total: packages.count,
+        per_page: per_page,
+        total_pages: Math.ceil(packages.count / per_page),
+        current_page: page
+      }
+
+      return Response.success(res, Message.success._success, packageData);
+    }
+
     const packages = await db.Package.findAll()
 
     return Response.success(res, Message.success._success, packages);
