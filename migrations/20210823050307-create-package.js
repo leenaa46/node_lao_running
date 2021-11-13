b4e400aac431e6d046c2361f72f89d495fce5c14
LeeNaa <hsherlock22@gmail.com>
2021-11-13 16:13:35 +0700

    body to text

--- migrations/20210823050307-create-package.js
+++ migrations/20210823050307-create-package.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       body: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
       },
 
       createdAt: {
--- models/packages.js
+++ models/packages.js
@@ -56,7 +56,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
     body: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     createdAt: {
       allowNull: false,
