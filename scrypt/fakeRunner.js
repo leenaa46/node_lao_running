e3d149464cda42e904117426c95d2f7c97cc0ce0
LeeNaa <hsherlock22@gmail.com>
2021-11-13 15:16:31 +0700

    Fix fake runner

--- scrypt/fakeRunner.js
+++ scrypt/fakeRunner.js
@@ -69,9 +69,10 @@ function createPackageResult(count, pack, encryptedPassword) {
       );
     });
 
+
     Promise.all(runnerResultFakers).then((runnerResults) => {
       const summary = runnerResults.reduce((pre, curr) => {
-        let total_range = pre.total_range + curr.dataValues.range;
+        let total_range = parseFloat(pre.total_range) + parseFloat(curr.dataValues.range);
         let total_time = pre.total_time + curr.dataValues.time;
         return {
           total_range,
@@ -81,6 +82,7 @@ function createPackageResult(count, pack, encryptedPassword) {
         total_range: 0,
         total_time: 0,
       });
+      console.log(summary);
       user.createRanking({
         total_range: summary.total_range,
         total_time: summary.total_time
